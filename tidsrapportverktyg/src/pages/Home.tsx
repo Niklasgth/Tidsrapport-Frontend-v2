4fa67053f2d68fc68d6c7bbd7d9d1f94a8539a08
Thorstone <Niklasgth@hotmail.com>
2025-05-09 21:24:29 +0200

    home pill

--- tidsrapportverktyg/src/pages/Home.tsx
+++ tidsrapportverktyg/src/pages/Home.tsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import styles from '@styles/home.module.css';
+import ButtonLink from '@components/ui/button/ButtonLink';
 
+const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Välkommen till Tidsrapportverktyget T.R.V!
+      </h1>
+      <p className={styles.subtitle}>
+        Håll koll på din tid med stil och precision.
+      </p>
+      <ButtonLink to="/loggedin">Börja kartlägga din tid</ButtonLink>
+    </div>
+  );
+};
 
-const Home = () => {
-    return (
-      <div>
-        <h1>Välkommen till Tidsrrapportverktyget T.R.V!</h1>
-      </div>
-    );
-  };
-  
-  export default Home;
-  
+export default Home;
